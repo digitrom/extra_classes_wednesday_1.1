e406f53cebb46bf4b10e8f7585499d15ca85853d
digitrom <roma.bobrovski@gmail.com>
2023-07-03 23:21:30 +0200

    extra_classes_wednesday_1.1 don

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,32 @@
+import {v1} from 'uuid';
+import {FilterValuesType, TodolistType} from "../App";
+
+
+export type AddTodolistActionType = ReturnType<typeof addTodolistAC>
+
+
+
+
+type ActionsType = AddTodolistActionType
+
+const initialState: Array<TodolistType> =  []
+
+export const todolistsReducer = (state: Array<TodolistType> = initialState, action: ActionsType): Array<TodolistType> => {
+    debugger
+    switch (action.type) {
+        case 'ADD-TODOLIST': {
+            debugger
+            return [{
+                id: action.todolistId,
+                title: action.title,
+                filter: 'all'
+            }, ...state]
+        }
+        default:
+            return state;
+    }
+}
+
+
+export const addTodolistAC = (title: string) => ({ type: 'ADD-TODOLIST', title: title, todolistId: v1()}) as const
+
